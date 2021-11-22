3b99f4c0ea79f1c5b2609742f5eef0a9183d3e07
lizguroart <digutierrezr@gmail.com>
2021-11-22 17:23:58 -0500

    3 commit

--- retos/src/main/java/com/proyecto/retos/app/controllers/UserController.java
+++ retos/src/main/java/com/proyecto/retos/app/controllers/UserController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.proyecto.retos.app.contollers;
+package com.proyecto.retos.app.controllers;
 
 import com.proyecto.retos.app.entities.User;
 import com.proyecto.retos.app.services.UserService;
